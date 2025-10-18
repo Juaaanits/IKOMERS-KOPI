8c2c6b8b3864a1f658e25dfb8e9738e2d708555b
Laurennn123 <2021-100715@rtu.edu.ph>
2025-10-18 17:50:41 +0800

    done nav bar

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Home | IKOMERS KOPI</title>
-<link rel="stylesheet" href="../assets/css/home-style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | IKOMERS KOPI</title>
+    <link rel="stylesheet" href="../assets/css/home-style.css">
 </head>
+
 <body>
-
-<h1>Hello World IKOMERS KOPI!</h1>
-
+    <nav>
+        <div id="navbar-holder">
+            <div id="navbar-items">
+                <header id="nav-title">
+                    <h1>Coffee</h1>
+                </header>
+                <div id="nav-items-pages">
+                    <a href="">Home</a>
+                    <a href="">Menu</a>
+                    <a href="">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 </body>
+
 </html>
