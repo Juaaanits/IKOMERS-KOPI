dbed05f2f87b363940dd6816e9b47216337a4cdc
Laurennn123 <2021-100715@rtu.edu.ph>
2025-10-19 17:24:08 +0800

    feat: done with the html for the 'welcome' section and the 'features/types of coffee' section

--- pages/home.php
+++ pages/home.php
@@ -10,7 +10,7 @@
 
 <body>
     <nav>
-        <div id="navbar-holder">
+        <div id="navbar-container">
             <div id="navbar-items">
                 <header id="nav-title">
                     <h1>Coffee</h1>
@@ -23,6 +23,56 @@
             </div>
         </div>
     </nav>
+
+    <main>
+        <section id="welcome-container">
+            <div id="welcome-container2">
+                <div id="welcome-info">
+                    <h1>Savor the Perfect Brew!</h1>
+                    <p>Press robust frappuccino Barista flavour eu java seasonal trifecta sit, dark sweet blue con
+                        Aftertaste aged organic foam, lungo medium shop At chicory skinny mazagran grinder.</p>
+                    <form method="" action="" id="welcome-buttons"> <!-- soon to fill depends of what purpose -->
+                        <button type="button" name="">Button1</button>
+                        <button type="button" id="button2" name="">Button2</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <section id="features-container">
+            <h1>Features</h1>
+            <div id="features-item">
+                <div class="coffee-item">
+                    <img src="../assets/images/coffee-latte-icon.png" alt="">
+                    <h1>Eapresso</h1>
+                    <p>Cortado At so seasonal Bar sweet, mountain caffeine viennese mocha as blue, mug percolator
+                        cappuccino robust.</p>
+                </div>
+                <div class="coffee-line"></div>
+                <div class="coffee-item">
+                    <img src="../assets/images/coffee-latte-icon.png" alt="">
+                    <h1>Latte</h1>
+                    <p>Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma cream Barista, spoon latte grinder
+                        extraction As press turkish mazagran est irish, affogato viennese breve and body so aromatic
+                        ristretto.</p>
+                </div>
+                <div class="coffee-line"></div>
+                <div class="coffee-item">
+                    <img src="../assets/images/coffee-latte-icon.png" alt="">
+                    <h1>Americano</h1>
+                    <p>Rich ut mazagran Aged whipped latte, breve instant beans spice press aftertaste, saucer espresso
+                        irish au.</p>
+                </div>
+                <div class="coffee-line"></div>
+                <div class="coffee-item">
+                    <img src="../assets/images/coffee-latte-icon.png" alt="">
+                    <h1>Cold Brew</h1>
+                    <p>Press Aromatic variety aroma black Brewed body latte And as mug, and seasonal affogato beans
+                        Arabica extra roast lait.</p>
+                </div>
+            </div>
+        </section>
+    </main>
+
 </body>
 
 </html>
