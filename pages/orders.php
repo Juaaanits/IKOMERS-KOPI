d09ddfd0384bdbdd6c90b3ee54ed5d41e6a2b365
Laurennn123 <2021-100715@rtu.edu.ph>
2025-11-23 22:54:07 +0800

    feat: initial design for order form

--- pages/orders.php
+++ pages/orders.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,6 +24,67 @@
                 <h1>Cafe Orders</h1>
             </header>
             <button id="add-order" type="button">+</button>
+            <dialog id="order-form">
+                <form action="" method="POST">
+                    <div id="dialog-order-form">
+                        <header id="order-header-container">
+                            <h2>Add New Order</h2>
+                            <button id="close-order-form" type="button">
+                                <img src="../assets/icons/close-window.png" alt="close-order-form"
+                                    id="close-order-form-button">
+                            </button>
+                        </header>
+                        <h3>Customer</h3>
+                        <div id="customer-container-input">
+                            <input type="text" class="customer" id="customer-field" name="customer"
+                                placeholder="Customer Name" disabled=true>
+                            <div class="customer" id="id-number">
+                                <p>ID: 87</p>
+                            </div>
+                            <button class="customer" id="search-customer" type="button">
+                                <img src="../assets/icons/search-interface-symbol.png" alt="search-customer"
+                                    id="search-customer-img">
+                            </button>
+                        </div>
+                        <h3>items</h3>
+                        <button id="add-item" type="button">Add Item</button>
+                        <table id="orders-customer-table">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <h4>Item</h4>
+                                    </th>
+                                    <th>
+                                        <h4>ID</h4>
+                                    </th>
+                                    <th>
+                                        <h4>Quantity</h4>
+                                    </th>
+                                    <th>
+                                        <h4>Price</h4>
+                                    </th>
+                                    <th>
+                                        <h4>Total</h4>
+                                    </th>
+                                </tr>
+                            </thead>
+                        </table>
+                        <div id="total-amount-container">
+                            <h3>Total</h3>
+                            <input type="text" id="total-amount-field" name="total-amount" placeholder="Total Amount"
+                                disabled=true>
+                        </div>
+                        <h3>Status</h3>
+                        <select name="order-status" id="order-status">
+                            <option value="pending">Pending</option>
+                        </select>
+                        <div id="cancel-add-container">
+                            <button class="cancel-add-order" id="cancel-order" type="button">Cancel</button>
+                            <button class="cancel-add-order" id="add-item-order" type="submit">Add Order</button>
+                        </div>
+                    </div>
+                </form>
+            </dialog>
         </div>
         <div id="filter-container">
             <input type="text" id="search-field-order" name="searchOrder" placeholder="Search orders">
@@ -82,9 +152,7 @@
 
     </main>
 
-
-
-
+    <script src="../assets/js/custom/orders.js"></script>
 </body>
 
 </html>
